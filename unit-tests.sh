584fe70e5a6a79417ec8299c65937e58a8473166
Kirill Krinkin <kirill.krinkin@gmail.com>
2012-07-28 02:37:57 +0400

    remove decoration

--- unit-tests.sh
+++ unit-tests.sh
@@ -5,26 +5,9 @@
 
 
 TESTS=`find ./tst/ -executable -type f`
-COUNT=`echo ${TESTS} | wc -w`
-
-
-echo "Found ${COUNT} test set(s): {"${TESTS}"}"
-rm -rf ./test.log
-rm -rf ./test_summary.log
 
 for test in ${TESTS}; 
 do
-	${test} | tee --append "./test.log"  
+	${test}   
 done;
-
-
-FAIL=`cat test.log | grep "FAIL" | wc -l`
-PASS=`cat test.log | grep "PASS" | wc -l`
-
-
-echo "" >>test_summary.log
-echo "See 'test.log' for results" >>test_summary.log
-echo "=================================================================">>test_summary.log
-echo "===  TESTING RESULTS Suites:${COUNT}   PASS/FAIL: ${PASS}/${FAIL}">>test_summary.log
-echo "=================================================================">>test_summary.log
 
